336737ef128a9628275095809b35d19dd95fb472
bocchi <boccfiap@outlook.com>
2025-06-03 00:44:15 -0300

    Adicinando a modelagem do banco de dados, @OneToMany para relacionamento de tabelas

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,6 +3,8 @@ package dev.java10x.cadastroDeNinjas.Missoes;
 import dev.java10x.cadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tb_missoes")
 public class MissoesModel {
@@ -10,20 +12,31 @@ public class MissoesModel {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String nome;
   private String dificuldade;
-  private NinjaModel ninja;
 
+  @OneToMany(mappedBy = "missoes") // @OneToMany - UNICA missao tem MUITOS ninjas
+  private List<NinjaModel> ninjas;
 
+  public MissoesModel(){}
 
+  public MissoesModel(String dificuldade, String nome) {
+    this.dificuldade = dificuldade;
+    this.nome = nome;
+  }
 
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-
-
-
-
-
-
-
-
+  public String getDificuldade() {
+    return dificuldade;
+  }
+  public void setDificuldade(String dificuldade) {
+    this.dificuldade = dificuldade;
+  }
 }
